f25b14767378a97c0ced7d5a35b197dd6469c1c8
Matheus Mendes <matheusmsf81@gmail.com>
2026-01-23 02:12:02 +0000

    Desafio nível Aventureiro terminado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char coluna_rotulo[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    char coluna_rotulo[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Rótulos das colunas
     int tabuleiro[10][10];
     int i, j;
 
@@ -12,38 +12,57 @@ int main() {
         }
     }
 
+    int tamanhoNavio = 3;
+
     // 2. Definição dos Navios (Tamanho 3)
     int navioH[3] = {3, 3, 3}; // Navio Horizontal
     int navioV[3] = {3, 3, 3}; // Navio Vertical
+    int navioD1[3] = {3, 3, 3}; // Navio Diagonal 1
+    int navioD2[3] = {3, 3, 3}; // Navio Diagonal 2
 
     // Coordenadas iniciais (Linha e Coluna)
     int linhaH = 2, colunaH = 1; // Navio Horizontal na linha 3 (índice 2)
     int linhaV = 5, colunaV = 5; // Navio Vertical na coluna F (índice 5)
+    int linhaD1 = 6, colunaD1 = 0; // Navio Diagonal 1
+    int linhaD2 = 1, colunaD2 = 7; // Navio Diagonal 2
 
     // Validação de Limites e Sobreposição
     int erro = 0;
 
     // Validar Navio Horizontal
-    if (colunaH < 0 || colunaH + 3 > 10 || linhaH < 0 || linhaH >= 10) {
+    if (colunaH < 0 || colunaH + tamanhoNavio > 10 || linhaH < 0 || linhaH >= 10) {
         printf("Erro: Navio Horizontal fora dos limites!\n");
         erro = 1;
     }
 
     // Validar Navio Vertical
-    if (linhaV < 0 || linhaV + 3 > 10 || colunaV < 0 || colunaV >= 10) {
+    if (linhaV < 0 || linhaV + tamanhoNavio > 10 || colunaV < 0 || colunaV >= 10) {
         printf("Erro: Navio Vertical fora dos limites!\n");
         erro = 1;
     }
 
+    // Validar Navio Diagonal 1
+    if (linhaD1 < 0 || linhaD1 + tamanhoNavio > 10 || colunaD1 < 0 || colunaD1 + tamanhoNavio > 10){
+        printf("Erro: Navio Diagonal 1 fora dos limites!\n");
+        erro = 1;
+    }
+
+    // Validar Navio Diagonal 2
+    if (linhaD2 < 0 || linhaD2 + tamanhoNavio > 10 || colunaD2 < 0 || colunaD2 - (tamanhoNavio - 1) < 0){
+        printf("Erro: Navio Diagonal 2 fora dos limites!\n");
+        erro = 1;
+    }
+    
+
     // Se dentro dos limites, posicionar e verificar sobreposição
     if (!erro) {
-        // Posicionar Horizontal
-        for (j = 0; j < 3; j++) {
+        // Posicionar Navio Horizontal
+        for (j = 0; j < tamanhoNavio; j++) {
             tabuleiro[linhaH][colunaH + j] = navioH[j];
         }
 
-        // Posicionar Vertical (verificando se já existe um navio (3) no local)
-        for (i = 0; i < 3; i++) {
+        // Posicionar Navio Vertical (verificando se já existe um navio (3) no local)
+        for (i = 0; i < tamanhoNavio; i++) {
             if (tabuleiro[linhaV + i][colunaV] == 3) {
                 printf("Erro: Os navios estao se sobrepondo!\n");
                 erro = 1;
@@ -51,6 +70,37 @@ int main() {
             }
             tabuleiro[linhaV + i][colunaV] = navioV[i];
         }
+
+        // Posiicionar Navio Diagonal 1 (verificando sobreposição)
+       for (i = 0; i < tamanhoNavio; i++){
+        int linha = linhaD1 + i;
+        int coluna = colunaD1 + i;
+
+        if (tabuleiro[linha][coluna] == 3) {
+            printf("Erro: Navio Diagonal Principal sobreposto a outro navio!\n");
+            erro = 1;
+            break;
+        }
+
+        tabuleiro[linha][coluna] = navioD1[i];
+       }
+       
+       // Posiicionar Navio Diagonal 2 (verificando sobreposição)
+       for (i = 0; i < tamanhoNavio; i++) {
+        int linha = linhaD2 + i;
+        int coluna = colunaD2 - i;
+
+        if (tabuleiro[linha][coluna] == 3) {
+            printf("Erro: Navio Diagonal Secundaria sobreposto a outro navio!\n");
+            erro = 1;
+            break;
+        }
+
+        tabuleiro[linha][coluna] = navioD2[i];
+        
+       }
+       
+        
     }
 
     // 3. Exibição do Tabuleiro
